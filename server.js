cbfed1e0e231db88e0b257584f155ee44e9dc67c
tylerccaceres <tyler.caceres@gmail.com>
2019-10-17 12:02:13 -0400

    modify port info

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
