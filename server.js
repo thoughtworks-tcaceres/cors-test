30ba73ffe18d2ad65b946dd37f195cef908f4f36
tylerccaceres <tyler.caceres@gmail.com>
2019-10-17 12:28:27 -0400

    cors fix

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Credentials", true);
   next();
@@ -16,7 +19,8 @@ app.get("/login", (req, res) => {
   res.send("hello world");
 });
 
-app.post("/getinfo", (req, res) => {
+app.get("/getinfo", (req, res) => {
+  console.log(req);
   res.json("data is here");
 });
 
