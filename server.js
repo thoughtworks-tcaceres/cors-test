6fe32c7386f79e3fe814fe15ec80a1fa7b4f93e0
tylerccaceres <tyler.caceres@gmail.com>
2019-10-17 11:47:39 -0400

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.get("/login", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/getinfo", (req, res) => {
+  res.json("data is here");
+});
+
+app.listen(PORT, () => {
+  console.log("server has started");
+});
